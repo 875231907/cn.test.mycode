56724634dd70265a58b161a9637bd17fa8671592
MaSheng <wb-ms752163@alibaba-inc.com>
2020-05-20 17:56:44 +0800

    提交测试

---
+++ myDemo/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @author MaSheng
+ * @version 1.0
+ * @date 2020/5/20 17:55
+ * @desc cn.test.mycode
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
